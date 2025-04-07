37b93b87eefef2fc86728c3d7eed61f692ddcb94
NickOsipov <nick.osipov.91@gmail.com>
2025-04-07 21:38:02 +0300

    feat: add math functions to src

---
+++ src/mathematics.py
@@ -0,0 +1,24 @@
+"""
+Module: mathematics.py
+Description: This module contains mathematical functions for various operations.
+"""
+
+from typing import List
+
+
+def sum_array(array: List[int, float]) -> int:
+    """
+    Function to sum an array of integers or floats.
+
+    Parameters
+    ----------
+    array : List[int, float]
+        Input array of integers or floats.
+
+    Returns
+    -------
+    int
+        Sum of the elements in the array.
+    """
+
+    return sum(array)
